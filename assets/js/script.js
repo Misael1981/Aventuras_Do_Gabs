7e1aad5884da79c84a26cd60625f9f2bae758d81
Misael Borges <ml_bbd@hotmail.com>
2025-01-11 18:56:54 -0300

    Finalizando home

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,30 +5,28 @@ const nav = document.querySelector(".nav"),
       allSection = document.querySelectorAll(".section"),
       totalSection = allSection.length;
 
-for(let i = 0; i < totalNavList; i++) {
+for (let i = 0; i < totalNavList; i++) {
+  const navItem = navList[i];
 
-    const a = navList[i].querySelector("a");
-    a.addEventListener("click", function() {
+  navItem.addEventListener("click", function() {
+    allSection.forEach(section => section.classList.remove("active"));
 
-        allSection.forEach(section => section.classList.remove("active")); 
+    const targetSection = document.querySelector(this.querySelector('a').getAttribute("href"));
+    targetSection.classList.add("active");
 
-        const targetSection = document.querySelector(this.getAttribute("href"));
-        targetSection.classList.add("active");
+    allSection.forEach(section => {
+      if (section !== targetSection) {
+        section.classList.add("back-section");
+      }
+    });
 
-        allSection.forEach(section => section.classList.remove("back-section")); 
+    navList.forEach(li => li.querySelector("a").classList.remove("active"));
 
-        allSection.forEach(section => {
-            if (section !== targetSection) {
-                section.classList.add("back-section");
-            }
-        });
+    this.querySelector("a").classList.add("active");
 
-        navList.forEach(li => li.querySelector("a").classList.remove("active"));
-
-        this.classList.add("active");
-
-        if (window.innerWidth < 1200) {
-            asideSectionToggleBtn(); 
-        }
-    })
+    // Fecha o menu lateral em telas menores (se necessário)
+    if (window.innerWidth < 1200) {
+      asideSectionToggleBtn(); 
+    }
+  });
 }
